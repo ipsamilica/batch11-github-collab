176c7e83994b8cc1773de91aa0e05d1421216e48
ipsamilica <ipsamilica@gmail.com>
2024-04-08 18:55:16 -0400

    added contact us page and a method

---
+++ src/com/techcenture/academy/pages/ContactUsPage.java
@@ -0,0 +1,9 @@
+package com.techcenture.academy.pages;
+
+public class ContactUsPage {
+    public ContactUsPage() {
+    }
+    public void contactUs(String message){
+        System.out.println("contacting with " + message);
+    }
+}
